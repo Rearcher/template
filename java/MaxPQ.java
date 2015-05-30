c2fea216950cc2719e05b87e9e2d88825742068d
rearcher <568813855@qq.com>
2015-05-30 20:00:46 +0800

    priority queue using heap

---
+++ java/MaxPQ.java
@@ -0,0 +1,70 @@
+/**
+ * Created by rahul on 5/30/15.
+ */
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N;
+
+    public MaxPQ(int capacity) {
+        pq = (Key[]) new Comparable[capacity+1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void insert(Key key) {
+        pq[++N] = key;
+        swim(N);
+    }
+
+    public Key delmax() {
+        Key max = pq[1];
+        exch(1, N--);
+        sink(1);
+        pq[N+1] = null;
+        return max;
+    }
+
+    public void show() {
+        for(Key i : pq)
+            System.out.print(i);
+    }
+
+    //When an element is added, it should swim up if the heap order is wrong.
+    private void swim(int k) {
+        while(k > 1 && less(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    //When the max element, the root of the heap, is removed, it exchange it with the last element,
+    //then the last element be the root place, it should sink down if the order is wrong.
+    private void sink(int k) {
+        while(2*k <= N) {
+            int j = 2*k;
+            if(j < N && less(j,j+1)) j++;
+            if(!less(k, j)) break;
+            exch(k,j);
+            k = j;
+        }
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+
+    public static void main(String[] args) {
+        MaxPQ<Integer> a = new MaxPQ(5);
+        for(int i = 1; i <= 5; i++)
+            a.insert(i);
+        a.show();
+    }
+}
