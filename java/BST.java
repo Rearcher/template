12e1ff3bda84f9e3479824188f41b8ad50125bf4
rearcher <568813855@qq.com>
2015-06-02 09:46:47 +0800

    Binary Search Tree

--- java/BST.java
+++ java/BST.java
@@ -92,9 +92,46 @@ public class BST<Key extends Comparable<Key>, Value> {
         else return 1+size(x.left)+rank(key,x.right);
     }//end of rank operation
 
-    public void delete(Key key) {
 
+    //delete operation
+    public void deleteMin() {
+        root = deleteMin(root);
     }
+
+    private Node deleteMin(Node x) {
+        if(x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    private Node min(Node x) {
+        while(x.left != null)
+            x = x.left;
+        return x;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = delete(x.left, key);
+        else if(cmp > 0) x.right = delete(x.right, key);
+        else {
+            if(x.right == null) return x.left;
+            if(x.left == null) return x.right;
+
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    }//end of delete operation
 
     //iterator
     public Iterable<Key> keys() {
